4f1850ef38f2fc58d8e5223a5350f9151b29975c
ManuelMac84 <manuelmac1984rgmail.com>
2026-05-08 12:51:14 +0200

    arreglando merge

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from src.sakila_ETL import test_connection, process_all_queries
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8475b2f922e7c373d266623ccc08410fa06ca745
 def main():
     test_connection()
     process_all_queries()
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8475b2f922e7c373d266623ccc08410fa06ca745
 if __name__ == "__main__":
     main()
